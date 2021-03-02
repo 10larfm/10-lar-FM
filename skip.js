51f5ae34ebe518b7daac1838295b7ecdeba495bb
10larfm <79935550+10larfm@users.noreply.github.com>
2021-03-02 21:56:16 +0300

    Update skip.js

--- skip.js
+++ skip.js
@@ -14,38 +14,38 @@ module.exports = {
       if (!channel)
         return message.channel.send(new MessageEmbed()
           .setColor(ee.wrongcolor)
-          .setAuthor("Bir ses kanalına katılmanız gerekiyor.", "https://cdn.discordapp.com/emojis/796283006280663060.png?v=1")
+          .setAuthor("Bir ses kanalına katılmanız gerekiyor.", "//EMOJİ URL//")
         );
       const player = client.manager.players.get(message.guild.id);
       if (!player)
         return message.channel.send(new MessageEmbed()
           .setColor(ee.wrongcolor)
-          .setAuthor("Hiçbir şey oynamıyor", "https://cdn.discordapp.com/emojis/796283006280663060.png?v=1")
+          .setAuthor("Hiçbir şey oynamıyor", "//EMOJİ URL//")
         );
       if (channel.id !== player.voiceChannel)
         return message.channel.send(new MessageEmbed()
           .setColor(ee.wrongcolor)
-          .setAuthor("Bu komutu kullanmak için benim ses kanalımda olmanız gerekiyor!", "https://cdn.discordapp.com/emojis/796283006280663060.png?v=1")
+          .setAuthor("Bu komutu kullanmak için benim ses kanalımda olmanız gerekiyor!", "//EMOJİ URL//")
           .setDescription(`Kanal ismi: \`${message.guild.channels.cache.get(player.voiceChannel).name}\``)
         );
       if (player.queue.size == 0) {
         if(player.get("otomatikoynatma")) return autoplay(client, player, "geç");
         player.destroy();
         return message.channel.send(new MessageEmbed()
-          .setAuthor("Durduruldu ve Kanalınızdan ayrıldı", "https://cdn.discordapp.com/emojis/796354949755437086.png?v=1")
+          .setAuthor("Durduruldu ve Kanalınızdan ayrıldı", "//EMOJİ URL//")
           .setColor(ee.color)
         );
       }
       player.stop();
       return message.channel.send(new MessageEmbed()
-        .setAuthor("Bir sonraki Şarkıya geçildi", "https://cdn.discordapp.com/emojis/796354952863678505.png?v=1")
+        .setAuthor("Bir sonraki Şarkıya geçildi", "//EMOJİ URL//")
         .setColor(ee.color)
       );
     } catch (e) {
         console.log(String(e.stack).bgRed)
         return message.channel.send(new MessageEmbed()
             .setColor(ee.wrongcolor)
-            .setAuthor(`Bir hata oluştu`, "https://cdn.discordapp.com/emojis/796283006280663060.png?v=1")
+            .setAuthor(`Bir hata oluştu`, "//EMOJİ URL//")
             .setDescription(`\`\`\`${e.stack}\`\`\``)
         );
     }
